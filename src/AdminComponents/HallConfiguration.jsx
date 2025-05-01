4999f00c5b58da5cffe674e5efa345bd15281d31
GensengG <168531024+GensengG@users.noreply.github.com>
2025-05-01 18:54:12 +0300

    Update HallConfiguration.jsx

--- src/AdminComponents/HallConfiguration.jsx
+++ src/AdminComponents/HallConfiguration.jsx
@@ -3,11 +3,7 @@ import "../App.css";
 import { useState } from "react";
 import { Sheme } from "./Sheme.jsx";
 
-// Логин - shfe-diplom@netology.ru
-// Пароль - shfe-diplom
-
 export const HallConfiguration = () => {    
-    
     function hideSection(e) {
         e.preventDefault();
         const sectionBody = document.getElementById("hall__config__body");
@@ -37,9 +33,7 @@ export const HallConfiguration = () => {
                 </button>
             ));
             setHalls(halls = hallElements);
-            // console.log(hallsResponse);
-        }
-        
+        }      
     ); 
 
     let hallRaws = 0;
@@ -54,254 +48,23 @@ export const HallConfiguration = () => {
             if(hallsResponse[i]["hall_name"] === hallName){
                 hallId = hallsResponse[i].id;
                 hallConfig = hallsResponse[i]["hall_config"];
-                // config = hallsResponse[i]["hall_config"];
                 hallRaws = hallsResponse[i]["hall_rows"];
                 hallPlaces = hallsResponse[i]["hall_places"];
                 setId(id = hallId);
                 setGrid({row: hallRaws, places: hallPlaces, config: hallConfig, id: hallId});
                 setFixedConfig({row: hallRaws, places: hallPlaces, config: hallConfig});
-                // sethallId(hallId = hallsResponse[i].id);
                 setConfig(config = <Sheme click = {grid}/>);
-                
             }
-        };
-        // for (let i = 0; i < hallConfig.length; i++) {
-        //     for (let j = 0; j < hallConfig[i].length; j++) {
-        //         if(hallConfig[i][j] === "standart"){
-        //             hallConfig[i][j] = <div className = "hall__place standart"></div>
-        //         } else if(hallConfig[i][j] === "vip"){
-        //             hallConfig[i][j] = <div className = "hall__place vip"></div>
-        //         } else if(hallConfig[i][j] === "taken"){
-        //             hallConfig[i][j] = <div className = "hall__place taken"></div>
-        //         } else {
-        //             hallConfig[i][j] = <div className = "hall__place disabled"></div>
-        //         } 
-        //     }
-        // }
-        
-        // setConfig(config = <Sheme click = {hallConfig}/>);
-        // console.log(hallConfig);
-        // setGrid({row: hallRaws, places: hallPlaces, config: hallConfig});
-        // console.log(grid);
-        // setOneHall(oneHall = hallConfig);
-        // hallConfig.map(item => (
-        //     <div key = {item.key}>
-        //         {item}
-        //     </div>
-        // ))
-        // setConfig(config = hallConfig.map(item => (
-        //     <div key = {item.key}>
-        //         {Object.values(item)}
-        //     </div>
-        // )));
-        // setConfig(config = hallConfig.map(item => (
-        //     <div key = {item.key}>
-        //         {item}
-        //     </div>
-        // )));
-        // return hallConfig;
-        // console.log(hallConfig);
-
-
-        // const params = new FormData()
-        // params.set('rowCount', hallRaws)
-        // params.set('placeCount', hallPlaces)
-        // params.set('config', JSON.stringify(hallConfig))
-        // fetch( `https://shfe-diplom.neto-server.ru/hall/${hallId}`, {
-        //     method: 'POST',
-        //     body: params 
-        // })
-        //     .then( response => response.json())
-            // .then( data => console.log( data ));
-            // .then( data => startConfig = data.result["hall_config"]);
-
-            
-        // let hallMatrix = [];
-
-        // function createHall(hallRaws, hallPlaces) {
-        //     hallMatrix = [];
-        //     for (let i = 0; i < hallRaws; i++) {
-        //         hallMatrix[i] = {};
-        //         for (let j = 0; j < hallPlaces; j++) {
-        //             let div = document.createElement('div');
-        //             if(hallConfig[i][j] === "standart"){
-        //                 div.className = 'hall__place standart';
-        //                 hallMatrix[i][j] = div;
-        //             } else if (hallConfig[i][j] === "vip"){
-        //                 div.className = 'hall__place vip';
-        //                 hallMatrix[i][j] = div;
-        //             } else if (hallConfig[i][j] === "taken"){
-        //                 div.className = 'hall__place taken';
-        //                 hallMatrix[i][j] = div;
-        //             } else {
-        //                 div.className = 'hall__place disabled';
-        //                 hallMatrix[i][j] = div;
-        //             }
-
-        //         }
-        //     }
-
-            // setConfig(config = {});
-        //     return hallMatrix;
-        // }
-
-        // function createHall(hallConfig) {
-        //     hallMatrix = [];
-        //     for (let i = 0; i < hallConfig.length; i++) {
-        //         hallMatrix.push(
-        //             []
-        //         )
-        //     }
-
-        //     for (let i = 0; i < hallConfig.length; i++) {
-        //         for (let j = 0; j < hallConfig[i].length; j++) {
-        //             let div = document.createElement('div');
-        //             if(hallConfig[i][j] === "standart"){
-        //                 div.className = 'hall__place standart';
-        //                 hallMatrix[i].push(div);
-        //             } else if (hallConfig[i][j] === "vip"){
-        //                 div.className = 'hall__place vip';
-        //                 hallMatrix[i].push(div);
-        //             } else if (hallConfig[i][j] === "taken"){
-        //                 div.className = 'hall__place taken';
-        //                 hallMatrix[i].push(div);
-        //             } else {
-        //                 div.className = 'hall__place disabled';
-        //                 hallMatrix[i].push(div);
-        //             }
-
-        //         }
-        //     }
-
-        //     // setConfig(config = {});
-        //     return hallMatrix;
-        // }
-
-        // console.log(createHall(hallRaws, hallPlaces));
-        // setHalls(...halls, halls.config = createHall(hallRaws, hallPlaces));
-        // let result = createHall(hallRaws, hallPlaces);
-        // render = () => {
-        //     let result = createHall(hallRaws, hallPlaces)
-        //     result.map(item => (
-        //     <div>
-        //         {item.forEach(element => {
-        //                 element;
-        //             })
-        //         }
-        //     </div>
-        // ))}
-        // co(createHall(hallConfig));
-        // setConfig(config = render.map(item => (
-        //     <div>
-        //         {item}
-        //     </div>
-        // )))
-
-        // render = createHall(hallConfig);
-        // console.log(render());
-
-        // startConfig = () => {
-            // render.forEach(item => {
-            //     console.log(item);
-
-            //     startConfig.push(
-            //         <div className = "AAAA">
-            //             {Object.values(item)}
-            //         </div>
-            //     )
-            // });
-        // }
-
-        // console.log(startConfig);
-
-    //     render.map(item => {
-    //         <div>
-    //             {item.forEach(elem => {
-    //                 return elem;
-    //             })}
-    //         </div>
-    //     });
-
-    //     setConfig(config = createHall(hallConfig));
-    //     console.log();
-    // }
-
-    // console.log(render);
-
-  
-    // fetch( 'https://shfe-diplom.neto-server.ru/alldata' )
-    // .then( response => response.json())
-    // .then( data => {
-    //         hallsResponse = data.result.halls;
-    //         for (let i = 0; i < hallsResponse.length; i++){
-    //             hallArr.push(hallsResponse[i]["hall_name"])
-    //         }
-    //         hallElements = hallArr.map(item => (
-    //             <div className = "hall__config" onClick = {hallStartConfig}>
-    //                {item}
-    //             </div>
-    //         ));
-    //         setHalls(halls = hallElements);
-    //     }
-    // );
-
+        }
     };
-
     let rowCount = 0;
     let placeCount = 0;
-    // let startPlaceCount = "";
-    // const fixedPlaces = 0;
-    // let newConfig = [];
-    // let start = document.getElementById("place");
-    // let startPlace = start.value;
-    // let startPlace = hallConfig[1].length;
-    // console.log(grid);
-
-    // function placeRowChange(){
-    //     let row = document.getElementById("row");
-    //     rowCount = row.value;
-    //     let place = document.getElementById("place");
-    //     placeCount = place.value;
-    //     newLayout = [];
-    //     for (let i = 0; i < rowCount; i++) { 
-    //         newLayout.push([]); 
-    //         for (let j = 0; j < placeCount; j++) {         
-    //             newLayout[i].push("standart")    
-    //         } 
-    //     }
-    //     // console.log(newLayout);
-    // }
-
-    
-    // function matrixGenerate(){
-    //     let placeArr = Array.from(document.getElementsByClassName("place"));
-    //     // console.log(placeArr);
-    //     let placeArrNew = placeArr.map(item => (
-    //         item.className
-    //     ));
-    //     // console.log(placeArrNew);
-    //     newConfig = [];
-    //     placeArr = placeArrNew.map(item => (
-    //         item.slice(6)
-    //     ));
-    //     // let startPlaces = config.props.click[0];
-    //     // let start = startPlaces.length;
-    //     console.log(placeArr);
-    //     for (let i = 0; i < start.value; i++) {               
-    //         newConfig.push(placeArr.slice(i * start.value, (i+1) * start.value));                
-    //     }
-    //     // console.log(start.value);
-    // }
-    // console.log(placeCount);
-
     function countRows(e) {
         e.preventDefault();
         rowCount = Number(e.target.value);
         placeCount = Number(document.getElementById("place").value);
         let newConfig = [];
         let newgrid = {};        
-        
-        // newConfig = [];
         let creatingRow = [];
         for(let i = 0; i < placeCount; i++){
             creatingRow.push("standart");
@@ -325,7 +88,6 @@ export const HallConfiguration = () => {
         rowCount = Number(document.getElementById("row").value);
         let newConfig = [];
         let newgrid = {};
-        // newConfig = [];
 
         let creatingRow = [];
         for(let i = 0; i < placeCount; i++){
@@ -363,8 +125,6 @@ export const HallConfiguration = () => {
             }
         }
         let placeCount = arrayConfig[0].length;
-
-
 
         const params = new FormData();
         params.set('rowCount', String(rowCount));
